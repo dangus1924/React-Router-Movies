aa8bf43a0d4dc41492c86990672aca81956abc37
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-09-04 20:26:38 -0400

    moved Movie card into MovieCard component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import MovieCard from './MovieCard'
 import axios from 'axios';
 
 const Movie = (props) => {
@@ -35,25 +36,11 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard key={movie.id} movie={movie} stars={movie.stars}/>
       <div className="save-button">Save</div>
     </div>
   );
 }
+
 
 export default Movie;
